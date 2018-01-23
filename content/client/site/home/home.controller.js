789e2ddf13c20c304d20f6418af08d4d3c4af09c
Jeslyn Leon <jeslynleon@gmail.com>
2018-01-23 09:33:52 -0800

    Removing comments

--- content/client/site/home/home.controller.js
+++ content/client/site/home/home.controller.js
@@ -1,15 +1,15 @@
-(function() {
+(function () {
     'use strict'
 
     angular.module('client.site')
         .controller('homeController', HomeController)
 
-    HomeController.$inject = ['messageService', 'blogPosts', '$log']
+    HomeController.$inject = ['messageService', 'blogPosts', '$http', '$log']
 
-    function HomeController(messageService, blogPosts, $log) {
+    function HomeController(messageService, blogPosts, $http, $log) {
         let vm = this
 
-        //Blog
+        //From previous blog implementation
         vm.blogPosts = null
         vm.leftPost = null
         vm.rightPost = null
@@ -17,13 +17,15 @@
         //Form
         vm.formData = null
         vm.submitMessage = _submitMessage
+        vm.sendMail = _sendMail
 
         init()
 
         function init() {
             $log.log('hola - you be home')
+
+            //Displaying blog posts
             vm.blogPosts = blogPosts
-            
             vm.leftPost = vm.blogPosts[0]
             vm.rightPost = vm.blogPosts[1]
         }
@@ -33,6 +35,19 @@
                 .then(data => $log.log(data))
                 .catch(err => $log.log(err))
         }
+
+        // function _sendMail() {
+        //     var data = vm.formData
+
+        //     //Post passing data
+        //     $http.post('/contact-form', data).
+        //         success(function (data, status, headers, config) {
+        //             window.alert('Thanks for your message!')
+        //         }).
+        //         error(function (data, status, headers, config) {
+        //             window.alert(data)
+        //         })
+        // }
 
     }
 })();
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const mongo = require('./app/mongodb')
 const router = require('./app/routes')
+//const nodeMailer = require('nodemailer')
 
 // initialize dotenv
 dotenv.config()
 
-// set our port
+// set port
 const port = process.env.PORT || 80
 
-// get all data/stuff of the body (POST) parameters
 // parse application/json
 app.use(bodyParser.json())
 
@@ -31,3 +31,32 @@ mongo.connect(process.env.MONGODB_URL)
         console.error(err)
         process.exit(1)
     })
+
+// NodeMailer config settings    
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail', 
+//     secure: false,
+//     port: 25,
+//     auth: {
+//         user: 'jeslynleon@gmail.com',
+//         pass: config.password
+//     },
+//     tls: {
+//         rejectUnauthorized: false
+//     }
+// })
+
+// let HelperOptions = {
+//     form: '"Jeslyn Leon" <jeslynleon@gmail.com',
+//     to: 'jeslynleon@gmail.com',
+//     subject: 'Did you see it?',
+//     text: 'Hell yea you did!'
+// }
+
+// transporter.sendMail(HelperOptions, (error, info) => {
+//     if(error) {
+//         console.log(error)
+//     } else {
+//         console.log(`The message was sent successfully! ${info}`)
+//     }
+// })
