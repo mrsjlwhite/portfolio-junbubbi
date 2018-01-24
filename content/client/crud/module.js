50af0726fded321ec2606bb973e17cdca79bb4ef
Jeslyn Leon <jeslynleon@gmail.com>
2018-01-23 17:11:52 -0800

    Updating ignore file

---
+++ content/client/crud/module.js
@@ -0,0 +1,18 @@
+/* global angular */
+(function () {
+    'use strict';
+
+    angular.module('client.crud', [])
+})();
+
+
+    // , ['ui.router'])
+
+    // angular.module('client.crud').config(RouteConfig)
+
+    // RouteConfig.$inject = ['$stateProvider']
+
+    // function RouteConfig($stateProvider) {
+    //     $stateProvider
+    //         .state
+
---
+++ content/client/layout/module.js
@@ -0,0 +1,21 @@
+/* global angular */
+(function() {
+    'use strict'
+
+    angular.module('client.layout', ['ui.router'])
+
+    angular.module('client.layout').config(RouteConfig)
+
+    RouteConfig.$inject = ['$stateProvider']
+
+    function RouteConfig($stateProvider) {
+        $stateProvider
+            .state('site', {
+                views: {
+                    root: {
+                        templateUrl: 'client/layout/layout.tpl.html'
+                    }
+                }
+            })
+    }
+})();
