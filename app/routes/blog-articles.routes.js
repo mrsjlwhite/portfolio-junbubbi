f9015224f78ee944799bdbb14dce9a543f7c95b9
Jeslyn Leon <jeslynleon@gmail.com>
2018-03-02 23:14:45 -0800

    applying same bug edits to other routes

--- app/routes/blog-articles.routes.js
+++ app/routes/blog-articles.routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const articleController = require('../controllers/blog-articles.controller')
-const validateBody = require('../filters/validate.body')
 const Article = require('../models/blog-article')
 
 module.exports = router
@@ -11,8 +10,8 @@ router.get('/', articleController.readAll)
 
 router.get('/:id([0-9a-fA-F]{24})', articleController.readById)
 
-router.post('/', validateBody(Article), articleController.create)
+router.post('/', articleController.create)
 
-router.put('/:id([0-9a-fA-F]{24})', validateBody(Article), articleController.update)
+router.put('/:id([0-9a-fA-F]{24})', articleController.update)
 
 router.delete('/:id([0-9a-fA-F]{24})', articleController.delete)
--- app/routes/blog-categories.routes.js
+++ app/routes/blog-categories.routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const categoryController = require('../controllers/blog-categories.controller')
-const validateBody = require('../filters/validate.body')
 const Category = require('../models/blog-category')
 
 module.exports = router
@@ -11,8 +10,8 @@ router.get('/', categoryController.readAll)
 
 router.get('/:id([0-9a-fA-F]{24})', categoryController.readById)
 
-router.post('/', validateBody(Category), categoryController.create)
+router.post('/', categoryController.create)
 
-router.put('/:id([0-9a-fA-F]{24})', validateBody(Category), categoryController.update)
+router.put('/:id([0-9a-fA-F]{24})', categoryController.update)
 
 router.delete('/:id([0-9a-fA-F]{24})', categoryController.delete)
