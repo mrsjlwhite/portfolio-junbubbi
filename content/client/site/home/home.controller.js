312544f3678af6b2072fc975c1d8e9458ebf5456
Jeslyn Leon <jeslynleon@gmail.com>
2018-03-04 15:04:48 -0800

    Removed home link and unused service, adjusted pic scripts.

--- content/client/site/home/home.controller.js
+++ content/client/site/home/home.controller.js
@@ -4,36 +4,15 @@
     angular.module('client.site')
         .controller('homeController', HomeController)
 
-    HomeController.$inject = ['messageService', 'blogPosts', '$http', '$log']
+    HomeController.$inject = ['$http', '$log']
 
-    function HomeController(messageService, blogPosts, $http, $log) {
+    function HomeController($http, $log) {
         let vm = this
-
-        // from previous blog implementation
-        vm.blogPosts = null
-        vm.leftPost = null
-        vm.rightPost = null
-
-        // from previous form
-        vm.formData = null
-        vm.submitMessage = _submitMessage
 
         init()
 
         function init() {
             $log.log('hola - you be home')
-
-            // displaying blog posts
-            vm.blogPosts = blogPosts
-            vm.leftPost = vm.blogPosts[0]
-            vm.rightPost = vm.blogPosts[1]
         }
-
-        function _submitMessage() {
-            messageService.create(vm.formData)
-                .then(data => $log.log(data))
-                .catch(err => $log.log(err))
-        }
-
     }
 })();
