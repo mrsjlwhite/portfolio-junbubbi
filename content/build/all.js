780f190e74e2403b27834ae5138a0323cde73ce8
Jeslyn Leon <jeslynleon@gmail.com>
2018-03-04 14:17:18 -0800

    commenting out unused front end services

--- content/build/all.js
+++ content/build/all.js
@@ -195,160 +195,189 @@ $(function () {
 (function () {
     'use strict';
 
-    angular.module('client.services').factory('blogPostService', BlogPostService);
+    angular.module('client.services');
+    // .factory('blogPostService', BlogPostService)
 
-    BlogPostService.$inject = ['$http', '$q'];
+    // BlogPostService.$inject = ['$http', '$q']
 
-    function BlogPostService($http, $q) {
-        return {
-            readAll: readAll,
-            readById: readById,
-            create: create,
-            update: update,
-            delete: _delete
-        };
+    // function BlogPostService($http, $q) {
+    //     return {
+    //         readAll: readAll,
+    //         readById: readById,
+    //         create: create,
+    //         update: update,
+    //         delete: _delete
+    //     }
 
-        function readAll() {
-            return $http.get('/api/myBlogPosts').then(function (dateChange) {
-                return convertAllDates(dateChange);
-            }).catch(onError);
-        }
+    //     function readAll() {
+    //         return $http.get('/api/myBlogPosts')
+    //         .then(dateChange => convertAllDates(dateChange))
+    //         .catch(onError)
+    //     }
 
-        function readById(id) {
-            return $http.get('/api/myBlogPosts/' + id).then(function (dateChange) {
-                return convertAllDates(dateChange);
-            }).catch(onError);
-        }
+    //     function readById(id) {
+    //         return $http.get(`/api/myBlogPosts/${id}`)
+    //         .then(dateChange => convertAllDates(dateChange))
+    //         .catch(onError)
+    //     }
 
-        function create(data) {
-            return $http.post('/api/myBlogPosts', data).then(xhrSuccess).catch(onError);
-        }
+    //     function create(data) {
+    //         return $http.post('/api/myBlogPosts', data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function update(data) {
-            return $http.put('/api/myBlogPosts/' + data._id, data).then(xhrSuccess).catch(onError);
-        }
+    //     function update(data) {
+    //         return $http.put(`/api/myBlogPosts/${data._id}`, data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function _delete(id) {
-            return $http.get('/api/myBlogPosts/' + id).then(xhrSuccess).catch(onError);
-        }
+    //     function _delete(id) {
+    //         return $http.get(`/api/myBlogPosts/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function xhrSuccess(response) {
-            return response.data;
-        }
+    //     function xhrSuccess(response) {
+    //         return response.data
+    //     }
 
-        function onError(error) {
-            console.log(error.data);
-            return $q.reject(error.data);
-        }
+    //     function onError(error) {
+    //         console.log(error.data)
+    //         return $q.reject(error.data)
+    //     }
 
-        function convertAllDates(dateChange) {
-            for (var x = 0; x < dateChange.data.items.length; x++) {
-                dateChange.data.items[x].published = new Date(dateChange.data.items[x].published);
-            }
-            return dateChange.data;
-        }
+    //     function convertAllDates(dateChange) {
+    //         for (let x = 0; x < dateChange.data.items.length; x++) {
+    //             dateChange.data.items[x].published = new Date(dateChange.data.items[x].published)     
+    //         }
+    //         return dateChange.data
+    //     }
 
-        function convertDate(dateChange) {
-            dateChange.data.item.published = new Date(dateChange.data.item.published);
-            return dateChange.data;
-        }
-    }
+    //     function convertDate(dateChange) {
+    //         dateChange.data.item.published = new Date(dateChange.data.item.published)
+    //         return dateChange.data
+    //     }
+    // }
 })();
 'use strict';
 
 (function () {
     'use strict';
 
-    angular.module('client.services').factory('blogCategoryServices', BlogCategoryService);
+    angular.module('client.services');
+    // .factory('blogCategoryServices', BlogCategoryService)
 
-    BlogCategoryService.$inject = ['$http', '$q'];
+    // BlogCategoryService.$inject = ['$http', '$q']
 
-    function BlogCategoryService($http, $q) {
-        return {
-            readAll: readAll,
-            readById: readById,
-            create: create,
-            update: update,
-            delete: _delete
-        };
+    // function BlogCategoryService($http, $q) {
+    //     return {
+    //         readAll: readAll,
+    //         readById: readById,
+    //         create: create,
+    //         update: update,
+    //         delete: _delete
+    //     }
 
-        function readAll() {
-            return $http.get('/api/blogCategories').then(xhrSuccess).catch(onError);
-        }
+    //     function readAll() {
+    //         return $http.get('/api/blogCategories')
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function readById(id) {
-            return $http.get('/api/blogCategories/' + id).then(xhrSuccess).catch(onError);
-        }
+    //     function readById(id) {
+    //         return $http.get(`/api/blogCategories/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function create(data) {
-            return $http.post('/api/blogCategories', data).then(xhrSuccess).catch(onError);
-        }
+    //     function create(data) {
+    //         return $http.post('/api/blogCategories', data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function update(data) {
-            return $http.put('/api/blogCategories/' + data._id, data).then(xhrSuccess).catch(onError);
-        }
+    //     function update(data) {
+    //         return $http.put(`/api/blogCategories/${data._id}`, data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function _delete(id) {
-            return $http.get('/api/blogCategories/' + id).then(xhrSuccess).catch(onError);
-        }
+    //     function _delete(id) {
+    //         return $http.get(`/api/blogCategories/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function xhrSuccess(response) {
-            return response.data;
-        }
+    //     function xhrSuccess(response) {
+    //         return response.data
+    //     }
 
-        function onError(error) {
-            console.log(error.data);
-            return $q.reject(error.data);
-        }
-    }
+    //     function onError(error) {
+    //         console.log(error.data)
+    //         return $q.reject(error.data)
+    //     }
+    // }
 })();
 'use strict';
 
 (function () {
     'use strict';
 
-    angular.module('client.services').factory('messageService', MessageService);
+    angular.module('client.services');
+    // .factory('messageService', MessageService)
 
-    MessageService.$inject = ['$http', '$q'];
+    // MessageService.$inject = ['$http', '$q']
 
-    function MessageService($http, $q) {
-        return {
-            readAll: readAll,
-            readById: readById,
-            create: create,
-            update: update,
-            delete: _delete
-        };
+    // function MessageService($http, $q) {
+    //     return {
+    //         readAll: readAll,
+    //         readById: readById,
+    //         create: create,
+    //         update: update,
+    //         delete: _delete
+    //     }
 
-        function readAll() {
-            return $http.get('/api/myMessages').then(xhrSuccess).catch(onError);
-        }
+    //     function readAll() {
+    //         return $http.get('/api/myMessages')
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function readById(id) {
-            return $http.get('/api/myMessages/' + id).then(xhrSuccess).catch(onError);
-        }
+    //     function readById(id) {
+    //         return $http.get(`/api/myMessages/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function create(data) {
-            return $http.post('/api/myMessages', data).then(xhrSuccess).catch(onError);
-        }
+    //     function create(data) {
+    //         return $http.post('/api/myMessages', data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function update(data) {
-            return $http.put('/api/myMessages/' + data._id, data).then(xhrSuccess).catch(onError);
-        }
+    //     function update(data) {
+    //         return $http.put(`/api/myMessages/${data._id}`, data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function _delete(id) {
-            return $http.delete('/api/myMessages/' + id).then(xhrSuccess).catch(onError);
-        }
+    //     function _delete(id) {
+    //         return $http.delete(`/api/myMessages/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function xhrSuccess(response) {
-            return response.data;
-        }
+    //     function xhrSuccess(response) {
+    //         return response.data
+    //     }
 
-        function onError(error) {
-            console.log(error.data);
-            return $q.reject(error.data);
-        }
-    }
+    //     function onError(error) {
+    //         console.log(error.data)
+    //         return $q.reject(error.data)
+    //     }
+    // }
 })();
 'use strict';
 
--- content/client/services/blog-category.service.js
+++ content/client/services/blog-category.service.js
@@ -2,56 +2,56 @@
     'use strict'
 
     angular.module('client.services')
-    .factory('blogCategoryServices', BlogCategoryService)
+    // .factory('blogCategoryServices', BlogCategoryService)
 
-    BlogCategoryService.$inject = ['$http', '$q']
+    // BlogCategoryService.$inject = ['$http', '$q']
 
-    function BlogCategoryService($http, $q) {
-        return {
-            readAll: readAll,
-            readById: readById,
-            create: create,
-            update: update,
-            delete: _delete
-        }
+    // function BlogCategoryService($http, $q) {
+    //     return {
+    //         readAll: readAll,
+    //         readById: readById,
+    //         create: create,
+    //         update: update,
+    //         delete: _delete
+    //     }
 
-        function readAll() {
-            return $http.get('/api/blogCategories')
-            .then(xhrSuccess)
-            .catch(onError)
-        }
+    //     function readAll() {
+    //         return $http.get('/api/blogCategories')
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function readById(id) {
-            return $http.get(`/api/blogCategories/${id}`)
-            .then(xhrSuccess)
-            .catch(onError)
-        }
+    //     function readById(id) {
+    //         return $http.get(`/api/blogCategories/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function create(data) {
-            return $http.post('/api/blogCategories', data)
-            .then(xhrSuccess)
-            .catch(onError)
-        }
+    //     function create(data) {
+    //         return $http.post('/api/blogCategories', data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function update(data) {
-            return $http.put(`/api/blogCategories/${data._id}`, data)
-            .then(xhrSuccess)
-            .catch(onError)
-        }
+    //     function update(data) {
+    //         return $http.put(`/api/blogCategories/${data._id}`, data)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function _delete(id) {
-            return $http.get(`/api/blogCategories/${id}`)
-            .then(xhrSuccess)
-            .catch(onError)
-        }
+    //     function _delete(id) {
+    //         return $http.get(`/api/blogCategories/${id}`)
+    //         .then(xhrSuccess)
+    //         .catch(onError)
+    //     }
 
-        function xhrSuccess(response) {
-            return response.data
-        }
+    //     function xhrSuccess(response) {
+    //         return response.data
+    //     }
 
-        function onError(error) {
-            console.log(error.data)
-            return $q.reject(error.data)
-        }
-    }
+    //     function onError(error) {
+    //         console.log(error.data)
+    //         return $q.reject(error.data)
+    //     }
+    // }
 })();
