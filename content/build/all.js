a55251a65a7d56a50199c8e1489951731cd06b70
Jeslyn Leon <jeslynleon@gmail.com>
2018-03-04 13:31:14 -0800

    updating url

--- content/build/all.js
+++ content/build/all.js
@@ -144,7 +144,7 @@
 
     function RouteConfig($stateProvider) {
         $stateProvider.state('site.home', {
-            url: '/home',
+            url: '/',
             views: {
                 'content': {
                     templateUrl: 'client/site/home/home.html',
