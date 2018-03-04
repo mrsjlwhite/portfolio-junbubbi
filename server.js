12fcba07a92c737f19b360e538d58a186a48260f
Jeslyn Leon <jeslynleon@gmail.com>
2018-03-04 14:29:48 -0800

    Removing env bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config({silent: true})
 const bodyParser = require('body-parser')
 const mongo = require('./app/mongodb')
 const router = require('./app/routes')
